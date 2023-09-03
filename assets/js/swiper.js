056dd0c45bc9c47d964eb2d634798fdbc7419775
Peggy Lin <stu90233@gmail.com>
2023-09-03 22:42:30 +0800

    add: faq

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,21 +1,11 @@
-// const swiper = new Swiper(".swiper", {
-//   loop: true,
-//   // 左右箭頭
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
-// const reviewSwiper = new Swiper(".reviewSwiper", {
-//   loop: true,
-//   slidesPerView: 3,
-//   spaceBetween: 24,
-//   pagination: {
-//     el: ".reviews-swiper-pagination",
-//     type: "progressbar",
-//   },
-// });
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  // 左右箭頭
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 const reviewSwiper = new Swiper(".reviewSwiper", {
   // loop: true,
